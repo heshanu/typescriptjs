c30138610edbcd91fc6ba91758ad87f8ca13352e
heshan umayanga <heshanu97@gmail.com>
2021-12-28 16:07:59 +0530

    tuesday

---
+++ src/app.js
@@ -0,0 +1,8 @@
+var Department = /** @class */ (function () {
+    function Department(n) {
+        this.name = n;
+    }
+    return Department;
+}());
+var HR = new Department("HR");
+console.log(HR);
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-// Code goes here!
+class Department{
+    name:string;
+   constructor(n:string){
+       this.name=n;
+   }
+   desctibe(){
+       console.log('Department: '+this.name);
+   }
+}
+let HR=new Department("HR");
+HR.desctibe();
+
+//const HRCOPY={describe:HR.de};
+HRCOPY.describe();
