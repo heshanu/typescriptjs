7e8275d2d3d2dbae2c3849cc6d3501f6feb9e5a4
heshan umayanga <heshanu97@gmail.com>
2021-12-28 12:23:31 +0530

    tuesday

--- app.ts
+++ app.ts
@@ -2,14 +2,34 @@
 const person:{
     name:string,age:number;
 }={
-*/
+
 const person={
     name:"ddsf",
-    age:23,hobbies:['Sports','Cooking']
+    age:23,hobbies:['Sports','Cooking'],
+    role:[1,2]
 }
 
 let favsp:any[];
 favsp=['ssss',1];
 
-console.log(person.name);
+
+const person={
+    name:'heshan',
+    age:24,
+    hobbies=['www','sdsdsds'],
+    role:
+}
+
+*/
+function combine(n1:number,n2:number):number{
+    if(typeof n1==='number'){}
+    const result=n2+n1;
+    return result;
+}
+
+let com:Function;
+com=combine;
+
+console.log(com(1,1));
+
 
